02b622c905556b538e23828f0c93bd9de45b30b6
sooriya11 <64911162+sooriya11@users.noreply.github.com>
2024-01-13 11:31:03 +0530

    Add files via upload

---
+++ week2/day3/Locators.java
@@ -0,0 +1,20 @@
+package week2.day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.id("username"));
+		
+		
+
+	}
+
+}
